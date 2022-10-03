edb7b5539273a4e9c0c00b8f2219a3fec2976ccb
Ini-obong Ukpong <iniobong10000@gmail.com>
2022-10-03 18:49:40 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import Navbar from "./components/Navbar";
+
+import Hero from "./views/Hero";
+import Works from "./views/Works";
+import Categories from "./views/Categories";
+import Recommendation from "./views/Recommendation";
+
+import Footer from "./views/Footer";
 
 function App() {
   return (
-    <div className="flex justify-center items-center text-center">
-    <p>Hollo world</p>
+    <div className="w-screen bg-[#FDF9F5] text-center flex flex-col items-center px-16">
+      <Navbar />
+      <Hero />
+      <Works />
+      <Categories />
+      <Recommendation />
+      <Footer />
     </div>
   );
 }
